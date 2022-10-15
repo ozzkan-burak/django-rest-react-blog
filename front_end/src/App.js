78a560572b6174108f74eeb3ec70611dd6e12964
burak.ozkan <burak.ozkan@flo.com.tr>
2022-10-15 22:19:44 +0300

    episode12

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+          <Switch>
+
+          </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
